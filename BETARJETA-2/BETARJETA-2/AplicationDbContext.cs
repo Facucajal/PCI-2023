30fb03f715f0d487f39a480d2fa113fec7ab91af
Facundo <facucajal9@gmail.com>
2023-06-25 03:45:24 -0300

    Avance BackEnd

---
+++ BETARJETA-2/BETARJETA-2/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BETARJETA_2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BETARJETA_2
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set;}
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options) {
+        }
+
+    }
+}
---
+++ BETARJETA-2/BETARJETA-2/Models/TarjetaCredito.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BETARJETA_2.Models
+{
+    public class TarjetaCredito
+    {
+        public int id { get; set; }
+        [Required]
+        public String Titular { get; set; }
+        [Required]
+        public String NumeroTarjeta { get; set; }
+        [Required]
+        public string FechaExpiracion { get; set;}
+        [Required]
+        public String CVV { get; set; }
+    }
+}
